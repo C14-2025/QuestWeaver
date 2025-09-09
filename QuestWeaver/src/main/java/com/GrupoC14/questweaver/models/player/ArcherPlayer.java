170772b26569eef9d79417f42482014098c8f27e
Felipe Tagawa <felipereisbkp@gmail.com>
2025-09-09 00:24:53 -0300

    feat: Class de Classes de player criadas e implementadas

---
+++ QuestWeaver/src/main/java/com/GrupoC14/questweaver/models/player/ArcherPlayer.java
@@ -0,0 +1,43 @@
+package com.GrupoC14.questweaver.models.player;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class ArcherPlayer extends RPGPlayer {
+
+    public ArcherPlayer(Player player) {
+        super(player, PlayerClass.ARCHER);
+    }
+
+    @Override
+    protected void initializeClass() {
+        // Stats iniciais do arqueiro
+        stats.setStrength(10);
+        stats.setDefense(8);
+        stats.setAgility(15);
+        stats.setIntelligence(7);
+        stats.setHealth(100);
+        stats.setMana(50);
+    }
+
+    @Override
+    public void levelUp() {
+        level++;
+        stats.setAgility(stats.getAgility() + 3);
+        stats.setStrength(stats.getStrength() + 2);
+        stats.setHealth(stats.getHealth() + 10);
+        stats.setMana(stats.getMana() + 5);
+        player.sendMessage("Arqueiro subiu para nível " + level + "!");
+    }
+
+    @Override
+    public ItemStack[] getStartingEquipment() {
+        return new ItemStack[] {
+                new ItemStack(Material.BOW, 1),
+                new ItemStack(Material.ARROW, 32),
+                new ItemStack(Material.LEATHER_CHESTPLATE, 1),
+                new ItemStack(Material.COOKED_CHICKEN, 5)
+        };
+    }
+}
---
+++ QuestWeaver/src/main/java/com/GrupoC14/questweaver/models/player/ClassManager.java
@@ -0,0 +1,7 @@
+package com.GrupoC14.questweaver.models.player;
+
+public class ClassManager {
+
+    // Esta classe deve implementar o gerenciamento e escolha de classes pelo jogador
+
+}
