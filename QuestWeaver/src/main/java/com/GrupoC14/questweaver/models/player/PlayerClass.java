11039e80243711af8e3b79e0007531df8cbfefb1
Felipe Tagawa <felipereisbkp@gmail.com>
2025-09-09 00:19:22 -0300

    feat: Criação da class enum de classes do sistema RPG

--- QuestWeaver/src/main/java/com/GrupoC14/questweaver/models/player/PlayerClass.java
+++ QuestWeaver/src/main/java/com/GrupoC14/questweaver/models/player/PlayerClass.java
@@ -1,4 +1,17 @@
 package com.GrupoC14.questweaver.models.player;
 
-public class PlayerClass {
+public enum PlayerClass {
+    WARRIOR("Guerreiro"),
+    ARCHER("Arqueiro"),
+    MAGE("Mago");
+
+    private final String displayName;
+
+    PlayerClass(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
