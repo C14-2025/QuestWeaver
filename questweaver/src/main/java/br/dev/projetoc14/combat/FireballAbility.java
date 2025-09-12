e8410b701ac891d34d6f987b08897f16608b8c42
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-09-12 19:53:27 -0300

    feat: cadiciona a classe de ability fireball e seu método para utilizá-la

--- questweaver/src/main/java/br/dev/projetoc14/combat/FireballAbility.java
+++ questweaver/src/main/java/br/dev/projetoc14/combat/FireballAbility.java
@@ -1,4 +1,30 @@
 package br.dev.projetoc14.combat;
 
-public class FireballAbility {
+import br.dev.projetoc14.player.RPGPlayer;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Fireball;
+
+public class FireballAbility extends Ability {
+
+    public FireballAbility() {
+        super("Bola de Fogo", 20, 5); // nome, custo de mana, cooldown em segundos
+    }
+
+    @Override
+    public void cast(RPGPlayer caster) {
+        // Cria uma fireball na frente do player
+        Location loc = caster.getEyeLocation();
+        Fireball fireball = caster.launchProjectile(Fireball.class);
+        fireball.setYield(2F); // tamanho da explosão
+        fireball.setIsIncendiary(false);
+
+        // Efeitos visuais/sonoros
+        caster.getWorld().playSound(loc, Sound.ENTITY_BLAZE_SHOOT, 1f, 1f);
+        caster.getWorld().spawnParticle(Particle.FLAME, loc, 20, 0.3, 0.3, 0.3, 0.01);
+
+        caster.sendMessage("🔥 Você lançou uma Bola de Fogo!");
+    }
+
 }
