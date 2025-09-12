889d7e925476e4cbc2d6bca38eb15a26d874e808
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-09-12 19:48:25 -0300

    feat: adicionar suporte a habilidades e métodos delegadores no RPGPlayer

--- questweaver/src/main/java/br/dev/projetoc14/player/RPGPlayer.java
+++ questweaver/src/main/java/br/dev/projetoc14/player/RPGPlayer.java
@@ -1,15 +1,23 @@
 package br.dev.projetoc14.player;
 
+import br.dev.projetoc14.combat.Ability;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class RPGPlayer {
 
-    protected Player player; // Este player já vem do bukkit
+    protected Player player; // player real do Bukkit
     protected PlayerClass playerClass;
     protected int level;
     protected int experience;
     protected PlayerStats stats;
+    protected List<Ability> abilities = new ArrayList<>();
 
     public RPGPlayer(Player player, PlayerClass playerClass, int level, int experience, PlayerStats stats) {
         this.player = player;
@@ -25,17 +33,44 @@ public abstract class RPGPlayer {
     }
 
     protected abstract void initializeClass();
-
     public abstract void levelUp();
     public abstract ItemStack[] getStartingEquipment();
-    public void addExperience(int exp) { this.experience += exp; }
 
+    public void addExperience(int exp) {
+        this.experience += exp;
+    }
+
+    // ==== Delegadores do Bukkit Player ==== //
+    public Location getEyeLocation() {
+        return player.getEyeLocation();
+    }
+
+    public <T extends Projectile> T launchProjectile(Class<T> projectileClass) {
+        return player.launchProjectile(projectileClass);
+    }
+
+    public World getWorld() {
+        return player.getWorld();
+    }
+
+    public void sendMessage(String message) {
+        player.sendMessage(message);
+    }
+
+    // ==== Abilities ==== //
+    public void addAbility(Ability ability) {
+        abilities.add(ability);
+    }
+
+    public List<Ability> getAbilities() {
+        return abilities;
+    }
+
+    // ==== Getters ==== //
     public Player getPlayer() { return player; }
     public PlayerClass getPlayerClass() { return playerClass; }
     public int getLevel() { return level; }
     public void setLevel(int level) { this.level = level; }
     public int getExperience() { return experience; }
     public PlayerStats getStats() { return stats; }
-
-
 }
