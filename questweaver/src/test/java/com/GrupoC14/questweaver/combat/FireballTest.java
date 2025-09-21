17f094e0a5dbbe558d0428cb77c0f467a55d86ef
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-09-21 18:21:50 -0300

    feat: adiciona testes com mock para dano da fireball

--- questweaver/src/test/java/com/GrupoC14/questweaver/combat/FireballTest.java
+++ questweaver/src/test/java/com/GrupoC14/questweaver/combat/FireballTest.java
@@ -1,30 +1,54 @@
 package com.GrupoC14.questweaver.combat;
+
 import br.dev.projetoc14.combat.Fireball;
+import br.dev.projetoc14.player.MagePlayer;
+import br.dev.projetoc14.player.RPGPlayer;
+import org.bukkit.entity.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class FireballTest {
 
     private Fireball fireball;
+    private RPGPlayer caster;
+    private RPGPlayer target;
 
     @BeforeEach
     public void setUp() {
         fireball = new Fireball();
+
+        // mocks básicos
+        Player mockCaster = mock(Player.class);
+        Player mockTarget = mock(Player.class);
+
+        caster = new MagePlayer(mockCaster);
+        target = new MagePlayer(mockTarget);
+
+        target.setHealth(100);
+        target.setCurrentHealth(80); // começa com 80 de vida
     }
 
-    // Testa se os valores da habilidade foram inicializados corretamente
     @Test
     public void testAbilityProperties() {
         assertEquals("Bola de Fogo", fireball.getName());
         assertEquals(20, fireball.getManaCost());
         assertEquals(5, fireball.getCooldown());
+        assertEquals(25, fireball.getDamage());
     }
 
-    // Teste para confirmar se o cast pode ser chamado sem erros
-    // TODO: Fazer usando Mock será necessário um player para castar a habilidade
+    @Test
+    public void testFireballAppliesDamage() {
+        fireball.applyDamage(caster, target);
+        assertEquals(55, target.getCurrentHealth()); // 80 - 25
+    }
 
-    // Teste para confirmar se o send message foi enviado com sucesso
-    // Todo: Fazer usando Mock será necessário um player para castar a habilidade
+    @Test
+    public void testFireballDoesNotGoBelowZero() {
+        target.setCurrentHealth(10);
+        fireball.applyDamage(caster, target);
+        assertEquals(0, target.getCurrentHealth());
+    }
 }
