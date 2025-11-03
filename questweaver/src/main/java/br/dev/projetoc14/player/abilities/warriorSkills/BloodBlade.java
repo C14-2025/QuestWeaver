7163489b8f66a264c65bfb5130b4a71ca82499bf
Felipe Tagawa <felipereisbkp@gmail.com>
2025-11-03 16:06:15 -0300

    feat: Add new Ability not implemented yet to warrior

---
+++ questweaver/src/main/java/br/dev/projetoc14/player/abilities/warriorSkills/BloodBlade.java
@@ -0,0 +1,17 @@
+package br.dev.projetoc14.player.abilities.warriorSkills;
+
+import br.dev.projetoc14.QuestWeaver;
+import br.dev.projetoc14.player.RPGPlayer;
+import br.dev.projetoc14.player.abilities.Ability;
+
+public class BloodBlade extends Ability {
+
+    public BloodBlade() {
+        super("Blood Blade", 20, 15);
+    }
+
+    @Override
+    protected void onCast(RPGPlayer caster) {
+
+    }
+}
