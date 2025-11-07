b7dcb78a291cf7a958f357c961deabb3b8bf2311
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-11-07 19:55:29 -0300

    feat: add implementation to methods onHit & Cast to new types of arrow

--- questweaver/src/main/java/br/dev/projetoc14/player/abilities/archerSkills/KnockbackArrow.java
+++ questweaver/src/main/java/br/dev/projetoc14/player/abilities/archerSkills/KnockbackArrow.java
@@ -2,14 +2,59 @@ package br.dev.projetoc14.player.abilities.archerSkills;
 
 import br.dev.projetoc14.player.RPGPlayer;
 import br.dev.projetoc14.player.abilities.Ability;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Arrow;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
 
-public class KnockbackArrow extends Ability {
-    public KnockbackArrow(String name, int manaCost, int cooldown) {
+public class KnockbackArrow extends Ability implements arrows {
+
+    private final int damage = 15;
+    private final Plugin plugin;
+
+    public KnockbackArrow(String name, int manaCost, int cooldown, Plugin plugin) {
         super(name, manaCost, cooldown);
+        this.plugin = plugin;
     }
 
     @Override
     protected void onCast(RPGPlayer caster) {
+        Location loc = caster.getEyeLocation();
+        Arrow arrow = caster.launchProjectile(Arrow.class);
+        arrow.setCritical(true);
+        arrow.setVelocity(arrow.getVelocity().multiply(1.5));
 
+        arrow.setMetadata("knockback_arrow", new FixedMetadataValue(plugin, true));
+        arrow.setMetadata("knockback_arrow_shooter", new FixedMetadataValue(plugin, caster.getUniqueId()));
+
+        caster.getWorld().playSound(loc, Sound.ENTITY_ARROW_SHOOT, 1f, 1.2f);
+        caster.getWorld().spawnParticle(Particle.CRIT, loc, 10, 0.2, 0.2, 0.2, 0.01);
+    }
+
+    @Override
+    public void onHit(ProjectileHitEvent event, RPGPlayer caster, RPGPlayer target) {
+        if (!(event.getEntity() instanceof Arrow arrow)) return;
+
+        Location hitLoc = arrow.getLocation();
+
+        // Efeitos visuais e sonoros
+        hitLoc.getWorld().spawnParticle(Particle.DAMAGE_INDICATOR, hitLoc, 20, 1, 1, 1, 0.05);
+        hitLoc.getWorld().playSound(hitLoc, Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK, 1.2f, 1.0f);
+
+        // Dano direto no alvo, se houver
+        if (target != null) {
+            int newHealth = target.getCurrentHealth() - damage;
+            if (newHealth < 0) newHealth = 0;
+            target.setCurrentHealth(newHealth);
+        }
+        arrow.remove();
+    }
+
+    @Override
+    public int getDamage() {
+        return damage;
     }
 }
