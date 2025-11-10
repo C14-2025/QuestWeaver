7a8a270abeb25da8c440ed11b401e0c2a011c470
Felipe Tagawa <felipereisbkp@gmail.com>
2025-11-10 15:36:36 -0300

    fix: ability listener of assassin refactor

--- questweaver/src/main/java/br/dev/projetoc14/player/abilities/assassinSkills/AbilityListener.java
+++ questweaver/src/main/java/br/dev/projetoc14/player/abilities/assassinSkills/AbilityListener.java
@@ -117,7 +117,7 @@ public class AbilityListener implements Listener {
 
         String displayName = PlainTextComponentSerializer.plainText()
                 .serialize(Objects.requireNonNull(meta.displayName()));
-        return displayName.equalsIgnoreCase("Lâmina Sombria");
+        return displayName.equalsIgnoreCase("Punhal Sombrio");
     }
 
     public String formatName(String nome) {
