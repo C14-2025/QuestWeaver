5e68637f690ae2893130ad36c4593c31c08d6f7f
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-11-15 17:09:02 -0300

    feat: addd new methods to give knockback arrow new effects

--- questweaver/src/main/java/br/dev/projetoc14/player/abilities/archerSkills/KnockbackArrow.java
+++ questweaver/src/main/java/br/dev/projetoc14/player/abilities/archerSkills/KnockbackArrow.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Arrow;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffectType;
 
 public class KnockbackArrow extends Ability implements arrows {
 
@@ -40,17 +41,101 @@ public class KnockbackArrow extends Ability implements arrows {
 
         Location hitLoc = arrow.getLocation();
 
-        // Efeitos visuais e sonoros
-        hitLoc.getWorld().spawnParticle(Particle.DAMAGE_INDICATOR, hitLoc, 20, 1, 1, 1, 0.05);
-        hitLoc.getWorld().playSound(hitLoc, Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK, 1.2f, 1.0f);
+        playHitEffects(hitLoc);
 
-        // Dano direto no alvo, se houver
         if (target != null) {
-            int newHealth = target.getCurrentHealth() - damage;
-            if (newHealth < 0) newHealth = 0;
-            target.setCurrentHealth(newHealth);
+            applyDamage(target);
+            applySlowness(target);
+            applyNausea(target);
+            applyShockwaves(caster, target);
         }
         arrow.remove();
+    }
+
+    private void applyDamage(RPGPlayer target) {
+        int newHealth = target.getCurrentHealth() - damage;
+        target.setCurrentHealth(Math.max(newHealth, 0));
+    }
+
+    private void playHitEffects(Location hitLoc) {
+        hitLoc.getWorld().spawnParticle(Particle.DAMAGE_INDICATOR, hitLoc, 20, 1, 1, 1, 0.05);
+        hitLoc.getWorld().playSound(hitLoc, Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK, 1.2f, 1.0f);
+    }
+
+    private void applySlowness(RPGPlayer target) {
+        target.getPlayer().addPotionEffect(
+                new org.bukkit.potion.PotionEffect(
+                        PotionEffectType.SLOWNESS,
+                        20, // 1 segundo
+                        1,
+                        false, false, false
+                )
+        );
+    }
+
+    private void applyNausea(RPGPlayer target) {
+        target.getPlayer().addPotionEffect(
+                new org.bukkit.potion.PotionEffect(
+                        PotionEffectType.NAUSEA,
+                        16, // 0.8s
+                        0,
+                        false, false, false
+                )
+        );
+    }
+
+    private void applyShockwavePush(RPGPlayer caster, org.bukkit.entity.Player bukkitTarget) {
+        if (caster == null) return;
+
+        org.bukkit.util.Vector push =
+                bukkitTarget.getLocation().toVector()
+                        .subtract(caster.getLocation().toVector())
+                        .normalize()
+                        .multiply(0.3);
+
+        bukkitTarget.setVelocity(bukkitTarget.getVelocity().add(push));
+    }
+
+    private void applyShockwaves(RPGPlayer caster, RPGPlayer target) {
+        final org.bukkit.entity.Player bukkitTarget = target.getPlayer();
+
+        plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+            int ticks = 0;
+
+            @Override
+            public void run() {
+                if (ticks >= 16 || !bukkitTarget.isOnline()) {
+                    plugin.getServer().getScheduler().cancelTask(this.hashCode());
+                    return;
+                }
+
+                applyShockwavePush(caster, bukkitTarget);
+                playShockwaveParticles(bukkitTarget);
+
+                if (ticks % 4 == 0) {
+                    playShockwaveSound(bukkitTarget);
+                }
+
+                ticks++;
+            }
+        }, 1L, 1L);
+    }
+
+    private void playShockwaveParticles(org.bukkit.entity.Player bukkitTarget) {
+        bukkitTarget.getWorld().spawnParticle(
+                Particle.CRIT,
+                bukkitTarget.getLocation().add(0, 1, 0),
+                4, 0.3, 0.3, 0.3, 0.01
+        );
+    }
+
+    private void playShockwaveSound(org.bukkit.entity.Player bukkitTarget) {
+        bukkitTarget.getWorld().playSound(
+                bukkitTarget.getLocation(),
+                Sound.BLOCK_ANVIL_LAND,
+                0.4f,
+                1.5f
+        );
     }
 
     @Override
