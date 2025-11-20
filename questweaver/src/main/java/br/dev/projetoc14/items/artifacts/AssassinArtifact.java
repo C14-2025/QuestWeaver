64777c3cc9c4318d93bdaa00d32f8bd12574cb51
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-11-20 19:11:53 -0300

    refactor: delete unused classes

--- questweaver/src/main/java/br/dev/projetoc14/items/artifacts/AssassinArtifact.java
+++
@@ -1,8 +0,0 @@
-package br.dev.projetoc14.items.artifacts;
-
-import br.dev.projetoc14.items.Item;
-import org.bukkit.inventory.ItemStack;
-
-public class AssassinArtifact extends ArcherArtifact {
-
-}
